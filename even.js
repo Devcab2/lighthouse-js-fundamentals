b62f8765e8351fd0f7293d85f304d6e09eee4743
Chris Bowyer <christianbowyer1996@gmail.com>
2022-04-28 07:36:50 -0600

    using functions to make calculations

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+/*const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11); "not needed" */
+
+console.log(isEven(10));
+console.log(isEven(11));
+// just call the function and plug in the number you want.
